ae477e81507ee1e6968e632e52cd969cc49e0b3a
Rimgaile Sinkeviciene <rimgaile.sinkeviciene@gmail.com>
2018-08-21 12:57:46 +0300

    Fetch venues from Foursqaure API

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    venues: []
+  }
   
   componentDidMount() {
     this.loadMap("https://maps.googleapis.com/maps/api/js?key=AIzaSyAcES-3fnf6KWMLzALjjuNv0VYMX9UmIZA&callback=initMap");
     window.initMap = this.initMap;
+    this.getVenues();
   }
 
   loadMap = (url) => {
@@ -19,9 +24,15 @@ class App extends Component {
 
   initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+      center: {lat: 54.6871555, lng: 25.2796514},
+      zoom: 15
     });
+  }
+
+  getVenues = () => {
+    fetch('https://api.foursquare.com/v2/venues/search?ll=54.6871555,25.2796514&client_id=Q4IHJEVQLQJ05AGABSEKEZGWTHGGURFQ2JW4AOZYHVXU5UIX&client_secret=XE00D13XAWZVJMPQEQNQYLJ3XUUYN3JEMFSYPVOTH0YSAQZ4&v=20180820')
+      .then(response => response.json())
+        .then(data => this.setState({ venues: data }));
   }
 
   render() {
