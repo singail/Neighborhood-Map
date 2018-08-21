27c4ee14dc3c476923841069b5d3d86c2c9c5b8b
Rimgaile Sinkeviciene <rimgaile.sinkeviciene@gmail.com>
2018-08-21 11:19:04 +0300

    Load the google map

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  componentDidMount() {
+    this.loadMap("https://maps.googleapis.com/maps/api/js?key=AIzaSyAcES-3fnf6KWMLzALjjuNv0VYMX9UmIZA&callback=initMap");
+    window.initMap = this.initMap;
+  }
+
+  loadMap = (url) => {
+    const script = window.document.createElement('script');
+    script.src = url;
+    script.setAttribute('async', '');
+    script.setAttribute('defer', '');
+    document.body.appendChild(script);
+  }
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main>
+        <div id='map'></div>
+      </main>
     );
   }
 }
